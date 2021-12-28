b1f2af2b8cdac0da8574a82a214d3d01490dbbc3
BRG4-IT <admin@brg4.at>
2021-12-28 15:05:25 +0100

    removed obsolete code.

--- choco-python/detect-python.ps1
+++ choco-python/detect-python.ps1
@@ -53,27 +53,3 @@ else {
 
 Write-Output "Everything seems fine."
 exit 0
-
-
-
-
-
-
-
-
-
-
-
-
-
-### a script exit code 0 signals intune "nothing to do"...
-
-If (-Not (Test-Path -Path "$ENV:SystemRoot\py.exe")) {
-    exit 1
-}
-$packageName = "python" 
-choco feature enable --name="'useEnhancedExitCodes'" -y
-choco list -e $packageName --local-only
-exit $LastExitCode
-
-
